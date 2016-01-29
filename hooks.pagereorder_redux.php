0184743862d59bf765f2295a969a7dc99d35989b
fraklo <fraklopez@gmail.com>
2016-01-28 20:04:53 -0800

    Initial rebuild

---
+++ hooks.pagereorder_redux.php
@@ -0,0 +1,145 @@
+<?php
+class Hooks_pagereorder_redux extends Hooks {
+
+  public $meta = array(
+     'name'       => 'Page Reorder Redux',
+     'version'    => '0.2.8',
+     'author'     => 'Frak Lopez',
+     'author_url' => 'http://fraklopez.com'
+   );
+
+
+  // Add css
+  public function control_panel__add_to_head() {
+    if (URL::getCurrent(false) === '/entries') {
+      $add_on = 'pagereorder_redux';
+
+      return '<link rel="stylesheet" href="/_add-ons/'. $add_on .'/css/' .
+              $add_on .'.css" media="all" type="text/css"></link>';
+    }
+  }
+
+
+  // JavaScript
+  public function control_panel__add_to_foot() {
+    if (URL::getCurrent(false) === '/entries') {
+      $add_on = 'pagereorder_redux';
+
+      return '<script type="text/javascript" src="/_add-ons/'. $add_on .
+              '/js/jquery.'. $add_on .'.js"></script>';
+    }
+  }
+
+
+  // Reorder hook
+  public function pagereorder_redux__reorder() {
+    $response = array(
+      'linkage' => null,
+      'message' => 'No order data received. Please try again.',
+      'status' => 'error'
+    );
+
+    // Get current user, to check if we're logged in.
+    if ( !Auth::isLoggedIn()) {
+      exit('Invalid Request');
+    }
+
+    // Get POST data from request.
+    $order = Request::post('order', false);
+
+    // Make sure we've got a response.
+    if (!$order) {
+      Log::error($response['message'], 'pagereorder_redux');
+      echo json_encode($response);
+      return false;
+    }
+
+    // Array of page order objects.
+    $page_order = json_decode($order);
+
+    if(isset($page_order[0]->url) && $page_order[0]->url != '') {
+      $entry_folder = implode('/', explode('/', $page_order[0]->url, -1));
+      $entry_folder = str_replace(Config::getSiteRoot(), '', $entry_folder);
+
+      $response = $this->order_set($page_order, $entry_folder);
+    } else {
+      $response['message'] = "The data submitted was invalid";
+    }
+
+    echo json_encode($response);
+    return true;
+  }
+
+
+
+  // Loops through pages and renames file to new order number
+  private function order_set($page_order, $entry_folder) {
+    $content_path = Config::getContentRoot();
+    $entries = Statamic::get_content_tree('/'. $entry_folder,
+                                          3, 5, false, true);
+    $result = Array(
+      'linkage' => null,
+      'message' => 'Page order saved successfully!',
+      'status' => 'success'
+    );
+
+    // Array to store the links of old data coupled with new data.
+    // We return this to the view so we can use JS to update the pathing on the page.
+    $links = Array();
+
+    // Loop over original folder structure and rename all folders to
+    // reflect the new order.
+    $entry_folder = implode('/', explode('/', $page_order[0]->url, -1));
+    $entry_folder = str_replace(Config::getSiteRoot(), '', $entry_folder);
+    foreach ($page_order as $page) {
+      $page_url = str_replace(Config::getSiteRoot(), '/', $page->url);
+      foreach ($entries as $entry) {
+
+        // Store original folder data.
+        $file_ext = pathinfo($entry['raw_url'], PATHINFO_EXTENSION);
+
+        // used to generate the old pathing info.
+        $old_slug   = $entry['slug'];
+
+        // Match on the URL to get the correct order result for this item.
+        if( $page_url == $entry['url'] ) {
+          break;
+        }
+      }
+
+      $slug = explode('/', $page->url);
+      $slug = preg_replace("/^\/(.+)/uis", "$1", end($slug));
+      $new_name = sprintf("%02d", ($page->index + 1)) .'.'. $slug;
+      $old_name = $old_slug;
+      $links[] = Array(
+        'old' => $old_name,
+        'new' => $new_name
+      );
+      $folder_path = $content_path ."/" .$entry_folder ."/";
+
+      // Generate pathing to pass to rename()
+      $new_path = $folder_path . $new_name .'.'. $file_ext;
+      $old_path = $folder_path . $old_name .'.'. $file_ext;
+      if ($new_path !== $old_path) {
+
+        // Check the old path actually exists and the new one doesn't.
+        if (File::exists($old_path) && !File::exists($new_path)) {
+          rename($old_path, $new_path);
+        } else {
+          $result['status'] = 'error';
+          $result['message'] = 'Aborting: Can\'t guarantee file integrity ' .
+                                'for folders '. $old_path .' & '. $new_path;
+          Log::error($result['message'], 'pagereorder_redux');
+          break;
+        }
+      }
+    }
+    if($result['status'] !== 'error') {
+
+      // No error, set links
+      $result['linkage'] = $links;
+    }
+
+    return $result;
+  }
+}
---
+++ js/jquery.pagereorder_redux.js
@@ -0,0 +1,198 @@
+;(function () {
+  "use strict";
+
+  var sortTimer = null,
+      flashBar = $('#status-bar'),
+      fixHelper, flashes, flashMessage, initialize,
+      orderGet, orderSubmit, tableInfoUpdate;
+
+  // Flash messages
+  flashes = {
+    success: _.template("<div id=\"flash-msg\" class=\"success\">\n" +
+            "<span class=\"icon\"></span>\n" +
+            "<span class=\"msg\"><%= message %></p>\n</div>"),
+    error: _.template("<div id=\"flash-msg\" class=\"error\">\n" +
+            "<span class=\"icon\"></span>\n" +
+            "<span class=\"msg\"><%= message %></p>\n</div>")
+  };
+
+  flashBar.on('flash', function(e, data) {
+    flashMessage(data);
+  });
+
+
+  // Shows flash message
+  flashMessage = function(data) {
+    var delay = 50,
+        existingMessage, html;
+
+    // Remove previous flash message if it exists
+    if($('#flash-msg').length) {
+      delay = 150;
+      existingMessage = $('#flash-msg');
+      existingMessage.stop(true).fadeOut(delay, function() {
+        existingMessage.remove();
+      });
+    };
+    html = flashes[data.status]({message: data.message});
+    flashBar.prepend(html);
+    $('#flash-msg').delay(delay)
+            .animate({'margin-top' : '0'}, 900, 'easeOutBounce')
+            .delay(3000)
+            .animate({'margin-top' : '-74px'},
+              900,
+              'easeInOutBack',
+              function() {
+                $(this).remove();
+              }
+            );
+  };
+
+
+  // Return a helper with preserved width of cells
+  fixHelper = function(e, ui) {
+    ui.children().each(function() {
+      $(this).width($(this).width());
+    });
+
+    return ui;
+  }
+
+
+  // Sets up the page
+  initialize = function() {
+    var form = $('form'),
+        updateButton = '<div class="btn btn-small set-sort js_set_sort">' +
+                          'Update Sort' +
+                        '</div>';
+
+    // Add update button at top and bottom of form
+    form.prepend(updateButton)
+              .find('.section').after(updateButton);
+
+    $('.js_set_sort').on('click', function(e) {
+      if(!$(this).hasClass('disabled')) {
+
+        // Scroll to top to be able to see message
+        $("html, body").animate({ scrollTop: "0px" });
+
+        // Add disabled class to prevent multiple submissions
+        $('.js_set_sort').addClass('disabled');
+        orderSubmit(orderGet(form));
+      }
+    });
+
+    // Set drag handle for rows
+    form.find('.checkbox-col input').before(
+      '<span class="drag-handle">&#9776;</span>'
+    );
+
+    // Set position class to later update Number column
+    form.find('td.title').next('td').addClass('position');
+    form.find('tbody').sortable({
+      // Helper ensures row width while dragging
+      helper: fixHelper
+    });
+  }
+
+
+  // Gets the new order
+  orderGet = function(form) {
+    var rows = form.find('tbody tr'),
+        result = [],
+        i, len, page, row, url;
+
+    // Loop through all rows
+    for (i = 0, len = rows.length; i < len; i++) {
+      page = $(rows[i]);
+
+      // Set values
+      result.push({
+        index: page.index(),
+        url: $.trim(page.find('a.entry-view').attr('href'))
+      });
+    }
+
+    return JSON.stringify(result);
+  }
+
+
+  // Submits the new order
+  orderSubmit = function(data) {
+    $.ajax("/TRIGGER/pagereorder_redux/reorder", {
+      type: 'POST',
+      data: {
+        order: data
+      },
+      complete: function(jqxhr) {
+        var flashObj = {
+              status: 'error',
+              message: 'There was an error saving your page order.' +
+                        'Please try again.'
+            },
+            response;
+
+        // Set message to the response we got back
+        try {
+          response = $.parseJSON(jqxhr.responseText);
+          flashObj.message = response.message;
+          if (response.status === "success" && (response.linkage != null)) {
+            flashObj.status = 'success';
+
+            // Everything good, update table info
+            tableInfoUpdate(response.linkage);
+          }
+        } catch (e) {
+          flashObj.message = 'There was an issue with your data.';
+        }
+
+        $('.js_set_sort').removeClass('disabled');
+        return flashBar.triggerHandler('flash', flashObj);
+      },
+      error: function(jqxhr, status, error) {
+        $('.js_set_sort').removeClass('disabled');
+
+        return flashBar.triggerHandler('flash', {
+          status: 'error',
+          message: 'There was an error saving your page order.' +
+                    'Please try again.'
+        });
+      }
+    });
+  };
+
+
+  // Updates the table with updated links
+  // also updates the Number column
+  tableInfoUpdate = function(links) {
+    var $tree = $('.simple-table'),
+        a, anchors, link, links, _i, _j, _len, _len1;
+
+    // Update links
+    for (_i = 0, _len = links.length; _i < _len; _i++) {
+      link = links[_i];
+      if (!(link.old !== link["new"])) {
+        continue;
+      }
+      anchors = $tree.find("a[href*='" + link.old + "']");
+      for (_j = 0, _len1 = anchors.length; _j < _len1; _j++) {
+        a = anchors[_j];
+        a.href = a.href.replace("" + link.old, "" + link["new"]);
+      }
+    }
+
+    // Update Number column with new order
+    $tree.find('.ui-sortable tr').each(function(index) {
+      var val = index + 1;
+      if( val.toString().length < 2 ) {
+        val = "0" + val;
+      }
+
+      return $(this).find('.position').html(val);
+    });
+  }
+
+
+  // Start the show
+  initialize();
+}).call(this);
